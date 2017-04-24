6c54e736094ebd9f65c6eb7c0db0561859b2ae17
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-04-25 00:05:21 +0700

    add index.php

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author jsetiady
+ *
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IF5122 - WisataKu</title>
+</head>
+<body>
+
+<h1>Welcome to WisataKu</h1>
+
+<h2>Go to WisataKu website</h2>
+<h2>API Documentation</h2>
+
+</body>
+</html>
