fa0ec2d209c45e76b63b8db400e9248842072e1e
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-04-25 17:08:09 +0700

    set project structure

--- index.php
+++ index.php
@@ -9,11 +9,37 @@
 <html>
 <head>
     <title>IF5122 - WisataKu</title>
+    <style type="text/css">
+	    .wisata-ku {
+			width: 91px;
+			height: 27px;
+			font-family: AvenirNext;
+			font-size: 20px;
+			font-weight: bold;
+			font-style: italic;
+		}
+		
+		.line {
+			height: 0px;
+			box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+			border: solid 1px #979797;
+		}
+		
+		.l {
+			width: 29px;
+			height: 18px;
+			font-family: BodoniOrnamentsITCTT;
+			font-size: 18px;
+		}
+    </style>
 </head>
 <body>
-
+<div>
+	<span class="l">f</span>
+	<span class="wisata-ku">WisataKu</span>
+</div>
+<hr/>
 <h1>Welcome to WisataKu</h1>
-
 <ol>
 	<li><a href="#">Go to WisataKu website</a></li>
 	<li><a href="#">WisataKu API Documentation</a></li>
