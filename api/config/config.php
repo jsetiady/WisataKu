963b7f3d6be5b43df6c16505b3f72454d07f850b
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-05-11 15:00:38 +0700

    add config file

---
+++ api/config/config.php
@@ -0,0 +1,7 @@
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "root";
+$config['db']['pass']   = "root";
+$config['db']['dbname'] = "jazzleme_wisataku";
