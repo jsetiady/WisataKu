42065c3b473715185f43812da08fac60a391aeef
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-05-12 10:51:44 +0700

    refactor model. moved to model directory

--- api/src/controller/App.php
+++ api/src/controller/App.php
@@ -1,6 +1,6 @@
 <?php
 namespace WisataKu\WisataKuAPI;
-require_once "model/Model.php";
+require "model/Model.php";
 
 class App
 {
@@ -9,14 +9,35 @@ class App
     
     public function __construct() {
         $app = new \Slim\App;
+        
+        //$data = $model->getLocations();
         $app->get('/', function ($request, $response) {
             $response->getBody()->write("WisataKu API v1.0");
             return $response;
         });
         
-        $this->tourPackageService($app);
-        $this->locationService($app);
+        //$this->tourPackageService($app);
         
+        //GET location/
+        $app->group('/location', function () {
+            $isValidId = function ($id) {
+                return (int)$id && $id > 0 && $id <= 10;
+            };
+
+            $this->map(['GET'], '', function ($request, $response) {
+                $model = new Model();
+                $data = $model->getLocations();
+                
+                return $response->withJson($data);
+            });
+            $this->get('/{id}', function ($request, $response, $args) {
+                if(todoIdValid($args['id'])) {
+                    return $response->withJson(['message' => "Tour Package ".$args['id']]);
+                }
+                return $response->withJson(['message' => 'Tour Package Not Found'], 404);    
+            });
+        });
+
         $this->app = $app;
     }
     
@@ -30,27 +51,6 @@ class App
             $this->map(['GET'], '', function ($request, $response) {
                 $model = new Model();
                 $data = $model->getTourPackages();
-                
-                return $response->withJson($data);
-            });
-            $this->get('/{id}', function ($request, $response, $args) {
-                if(todoIdValid($args['id'])) {
-                    return $response->withJson(['message' => "Tour Package ".$args['id']]);
-                }
-                return $response->withJson(['message' => 'Tour Package Not Found'], 404);    
-            });
-        });
-    }
-    
-    public function locationService() {
-        $app->group('/location', function () {
-            $isValidId = function ($id) {
-                return (int)$id && $id > 0 && $id <= 10;
-            };
-
-            $this->map(['GET'], '', function ($request, $response) {
-                $model = new Model();
-                $data = $model->getLocations();
                 
                 return $response->withJson($data);
             });
