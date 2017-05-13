5db71d36df78ec9ef3aea58fd6746a002648e1e6
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-05-14 02:21:27 +0700

    Refactor TourPackage

--- api/src/model/TourPackage.php
+++ api/src/model/TourPackage.php
@@ -221,17 +221,6 @@ class TourPackage {
     {
     	return $this->tourItinerary;
     }
-
-    function utf8ize($d) {
-        if (is_array($d)) {
-            foreach ($d as $k => $v) {
-                $d[$k] = $this->utf8ize($v);
-            }
-        } else if (is_string ($d)) {
-            return utf8_encode($d);
-        }
-        return $d;
-    }
     
     public function toArray() {
         $util = new Util();
