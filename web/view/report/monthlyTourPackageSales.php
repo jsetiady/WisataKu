2fa9c6445db16a0986fa83f7f2488821eccb098c
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-05-16 02:47:17 +0700

    fix user interface in monthly tour package sales report

--- web/view/report/monthlyTourPackageSales.php
+++ web/view/report/monthlyTourPackageSales.php
@@ -1,54 +1,74 @@
 <?php
-    include(ABS_PATH."templates/_header.php");
+ include(ABS_PATH."templates/_header.php");
 ?>
+	<script type="text/javascript">
+		$("document").ready(function(){
+			$("#report-tour-sales-table").dataTable();
 
-<div class="py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4>Monthly Tour Package Sales Report</h4>
-            </div>
-        </div>
-      <div class="row">
-        <div class="col-md-12">
-          <table class="table">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Month</th>
-                <th>Year</th>
-                <th>Number of Transaction</th>
-                <th>Number of Confirmed Transaction</th>
-                <th>Total Nominal of Transaction</th>
-                <th>Total Nominal of Paid Transaction</th>
-              </tr>
-            </thead>
-            <tbody>
-                <?php
-                $i = 1;
-                foreach($transactions as $transaction):
-                ?>
-              <tr>
-                <td><?=$i?></td>
-                <td><?=$transaction["month"]?></td>
-                <td><?=$transaction["year"]?></td>
-                <td><?=$transaction["numTransaction"]?></td>
-                <td><?=$transaction["numConfirmTransaction"]?></td>
-                  <?php setlocale(LC_MONETARY, 'it_IT'); ?>
-                <td><?=str_replace("Eu", "Rp", money_format('%.2n', $transaction["totalPrice"]))?></td>
-                <td><?=str_replace("Eu", "Rp", money_format('%.2n', $transaction["totalConfirmedPrice"]))?></td>
-              </tr>
-                <?php
-                    $i++;
-                  endforeach;
-                ?>
-            </tbody>
-          </table>
+			$("#tour-transaction-btn").click(function(){
+				$("#tour-history-tab").show();
+				$("#souvenir-history-tab").hide();
+			});
+
+			$("#souvenir-transaction-btn").click(function(){
+				$("#tour-history-tab").hide();
+				$("#souvenir-history-tab").show();
+			});
+		});
+	</script>
+    <div class="py-5">
+      <div class="container">
+        <div class="row" style="margin:auto">
+          <div class="col-md-12" style="margin:auto;text-align:center;">
+          	<h5 style="margin:auto;"><b>Monthly Tour Package Sales Report</b></h5>
+          	<hr/>
+          </div>
+           <br/><br/>
+          <div id="tour-history-tab" class="col-md-12 form-control-sm" style="margin:auto;border:1px solid black;">
+          	<p><b>Monthly Tour Package Sales Report</b></p>
+          	<hr style="margin-top:5px;"/>
+          	
+          	<table class="form-control-sm" id="report-tour-sales-table">
+          		<thead>
+	          		<head>
+	          			<tr>
+                            <th>#</th>
+                            <th>Month</th>
+                            <th>Year</th>
+                            <th>Number of Transaction</th>
+                            <th>Number of Confirmed Transaction</th>
+                            <th>Total Nominal of Transaction</th>
+                            <th>Total Nominal of Paid Transaction</th>
+	          				
+	          			</tr>
+	          		</head>
+                </thead>
+	          		<tbody>
+                        <?php
+                        $i = 1;
+                        foreach($transactions as $transaction):
+                        ?>
+                            <tr>
+                                <td><?=$i?></td>
+                                <td><?=$transaction["month"]?></td>
+                                <td><?=$transaction["year"]?></td>
+                                <td><?=$transaction["numTransaction"]?></td>
+                                <td><?=$transaction["numConfirmTransaction"]?></td>
+                                  <?php setlocale(LC_MONETARY, 'it_IT'); ?>
+                                <td><?=str_replace("Eu", "Rp", money_format('%.2n', $transaction["totalPrice"]))?></td>
+                                <td><?=str_replace("Eu", "Rp", money_format('%.2n', $transaction["totalConfirmedPrice"]))?></td>
+                            </tr>
+                            <?php
+                            $i++;
+                            endforeach;
+                            ?>
+          		    </tbody>
+          	</table>
+          </div>
+          
         </div>
       </div>
     </div>
-  </div>
-
 <?php
-include(ABS_PATH."templates/_footer.php");
+    include(ABS_PATH."templates/_footer.php");
 ?>
