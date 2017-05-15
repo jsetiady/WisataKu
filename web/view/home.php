5ece392ecebfb546b15bbdd02fca4e626a10fa94
Jessie Setiady <23516064@std.stei.itb.ac.id>
2017-05-15 19:14:40 +0700

    fix issue in image url in home page, and fix ads image viewer

--- web/view/home.php
+++ web/view/home.php
@@ -25,7 +25,8 @@
   <div class="col-md-4">
     <div class="thumbnail">
       <a href="?cont=tour&action=detail&id=<?= $tour->getTourId() ?>">
-        <img src="../assets/images/1.jpg" alt="Tour Package" style="width:100%"> </a>
+        <img src="<?=$this->imageurl.$tour->getTourImageFilename()?>" alt="Tour Package" style="width:100%">
+      </a>
         <hr/>
         <?php
         	$tourType = "Personal";
